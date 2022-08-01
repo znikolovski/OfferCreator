a1045494483894c480991c664a557257921b3e71
Mark Szulc <mszulc@adobe.com>
2022-08-01 21:21:19 +1000

    Target API

---
+++ src/dx-excshell-1/actions/getAudiences/index.js
@@ -0,0 +1,58 @@
+const fetch = require('node-fetch')
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main (params) {
+  // create a Logger
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+  try {
+    // 'info' is the default level if not set
+    logger.info('** GET AUDIENCES **')
+
+    // log parameters, only if params.LOG_LEVEL === 'debug'
+    logger.debug(stringParameters(params))
+
+    // check for missing request input parameters and headers
+    const requiredParams = [/* add required params */]
+    const requiredHeaders = ['Authorization']
+    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    if (errorMessage) {
+      // return and log client errors
+      return errorResponse(400, errorMessage, logger)
+    }
+
+    // extract the user Bearer token from the Authorization header
+    const token = getBearerToken(params)
+    const apiEndpoint = 'https://mc.adobe.io/mark-szulc/target/audiences';
+    
+    // fetch content from external api endpoint
+    const res = await fetch(apiEndpoint, {
+        method: 'get',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'X-Api-Key': '070fd39573fc4798b261c517a63d858d'
+        }})
+    if (!res.ok) {
+      throw new Error('request to ' + apiEndpoint + ' failed with status code ' + res.status)
+    }
+    const content = await res.json();
+  
+    const response = {
+      statusCode: 200,
+      body: content
+    }
+
+    // log the response status code
+    logger.info(`${response.statusCode}: successful request`)
+    return response
+  } catch (error) {
+    // log any server errors
+    logger.error(error)
+    // return with 500
+    return errorResponse(500, 'server error', logger)
+  }
+}
+
+exports.main = main

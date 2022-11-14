d9520a163d020f148a75e875ed2e5266c04df4c0
Mark Szulc <mszulc@adobe.com>
2022-11-14 23:18:21 +1100

    Copy & Rename XF correctly

--- src/dx-excshell-1/actions/copyXFandRename/index.js
+++ src/dx-excshell-1/actions/copyXFandRename/index.js
@@ -27,34 +27,102 @@ async function main (params) {
     const token = getBearerToken(params)
     const apiEndpoint = params.AEM_AUTHOR + params.AEM_OFFER_ENDPOINT;
 
-    xfName = "foo";
-   
+    xfName = params.xfname;
+    xfTitle = params.xfname;
+    xfDescription = params.xfdescription;
+
     //START Copy JCR Node
     const copyNodeEndpoint =  params.AEM_AUTHOR + params.AEM_XF_SOURCE_PATH;
-    const postParams = new URLSearchParams();
-    postParams.append(':operation', 'copy');
-    postParams.append(':dest', params.AEM_XF_DESTINATION_PATH + xfName);
+    logger.info('DEST: ' + params.AEM_XF_DESTINATION_PATH + xfName);
+    logger.info('SOURCE: ' + copyNodeEndpoint);
+    
 
     const res = await fetch(copyNodeEndpoint, {
       method: 'post',
       headers: {
           'Authorization': 'Bearer ' + token,
-          'content-type': 'application/json'
-
+          'content-type': 'application/x-www-form-urlencoded'
       },
-      body: postParams
+      body: new URLSearchParams({
+        ':operation': 'copy',
+        ':dest': params.AEM_XF_DESTINATION_PATH + xfName
+      })
     })
 
     if (!res.ok) {
       throw new Error('request to ' + copyNodeEndpoint + ' failed with status code ' + res.status)
     }
-    const content = await res.json();
+   
     //END Copy JCR Node
+
+    //START Modify XF to point to CF
+    const xfVariations = params.AEM_XF_VARIATIONS_LIST.split(',');
+    xfVariations.forEach(async variation => {
+        const variationEndpoint = params.AEM_AUTHOR + params.AEM_XF_DESTINATION_PATH + xfName + '/' + variation + '/jcr:content/root//contentfragment'
+        logger.info('VARIATION: ' + variationEndpoint);
+        const postParams = new URLSearchParams();
+        postParams.append('fragmentPath', "/content/dam/frescopa/en/offers/" + xfName)
+
+        const res = await fetch(variationEndpoint, {
+            method: 'post',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'content-type': 'application/x-www-form-urlencoded'
+      
+            },
+            body: postParams
+        })
+        if (!res.ok) {
+            throw new Error('request to ' + variationEndpoint + ' failed with status code ' + res.status)
+        }
+    });
+    //END Modify XF to point to CF
+
+
+    //START Change XF Title and Description
+    const xfEndpoint = params.AEM_AUTHOR + params.AEM_XF_DESTINATION_PATH + xfName + '/jcr:content'
+    logger.info('xfEndpoint: ' + xfEndpoint);
+    
+    const xfRes = await fetch(xfEndpoint, {
+        method: 'post',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          'jcr:title': xfTitle,
+          'jcr:description': xfDescription
+        })
+    })
+    if (!xfRes.ok) {
+        throw new Error('request to ' + xfEndpoint + ' failed with status code ' + xfRes.status)
+    }
+    //END 
+
+    //START Modify CF metadata
+    const offerFragmentEndpoint = params.AEM_AUTHOR + "/content/dam/frescopa/en/offers/" + xfName + '/jcr:content/metadata'
+    const offerPostParams = new URLSearchParams();
+    offerPostParams.append('processed', 'true')
+
+    const offerRes = await fetch(offerFragmentEndpoint, {
+        method: 'post',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          'xfcreated': 'true'
+        })
+    })
+    if (!offerRes.ok) {
+        throw new Error('request to ' + variationEndpoint + ' failed with status code ' + res.status)
+    }
+    //END Modify CF metadata
 
   
     const response = {
       statusCode: 200,
-      body: content
+      body: "Good"
     }
 
     // log the response status code
